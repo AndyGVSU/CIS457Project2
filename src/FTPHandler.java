d214e5fde5145dcf170f1b799b7b041bd4122552
AndyGVSU <hudsoand@mail.gvsu.edu>
2019-10-28 12:30:08 -0400

    Threaded FTP Server/Client for later GUI use, completed request command, packaged code

---
+++ src/FTPHandler.java
@@ -0,0 +1,18 @@
+package cis457proj2_src;
+
+import cis457proj2_src.*;
+
+public class FTPHandler {
+
+	public static void main() {
+		FTPClient client = new FTPClient();
+		FTPServer server = new FTPServer();
+
+		Thread clientThread = new Thread(client);
+		Thread serverThread = new Thread(server);
+
+		//run client/server in a thread for GUI
+		clientThread.run();
+		serverThread.run();
+	}
+}
